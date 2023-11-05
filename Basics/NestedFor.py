52693fafb58c4449d3cb6b76ff675a53e224c498
VikasKare <karevikas7777@gmail.com>
2023-11-05 19:28:02 +0530

    Basics

---
+++ Basics/NestedFor.py
@@ -0,0 +1,46 @@
+
+
+
+# print this pattern 5 5 5 5 
+
+# x = 4
+# num = 5
+# while x >= 1:
+#     print(num,end=" ")
+#     x -= 1
+
+# print this pattern 
+
+# 1 * 3 * 5 * 7 * 9 * 
+
+x = 10
+
+for i in range(1,x+1):
+
+    if(i % 2 == 0):
+        print(end= " * ")
+    else:
+        print(i, end="")
+
+
+# in single lin
+print()
+
+z = 10
+for k in range( 1, z + 1, 2):
+    print(k, end=" * ")
+
+
+# print this pattern 
+
+# 5 * 15 * 25 * 35 * 45 * 
+
+print()
+
+y = 50
+
+for a in range(5, y, 10):
+    print(a, end=" * ")
+    
+print()
+print(a)
---
+++ Basics/NestedFor3.py
@@ -0,0 +1,28 @@
+
+'''
+for i in range(3):
+
+    print("In i for Loop")
+
+    for j in range(3):
+        print("In j for loop")
+
+
+
+for i in range(3):
+    print("in i for loop")
+
+    for j in range(i):
+
+        print("in j for loop")
+
+'''
+
+for i in range(3):
+
+    print("in i for loop")
+
+    for j in range(i + 1):
+        
+        print("in j for loop")
+
