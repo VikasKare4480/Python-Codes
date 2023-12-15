5131af3bbf46cc98211310ff5fe89d0d5412bcca
VikasKare <karevikas7777@gmail.com>
2023-12-15 20:55:14 +0530

    Classes and Objects

---
+++ Classes and Objects/Program1.py
@@ -0,0 +1,24 @@
+
+
+class Company:
+
+    def disp(self):
+        print('In Company disp ')
+
+
+def fun():
+    print("In fun ")
+
+
+print("Start the Code")
+
+# disp() 
+
+fun()
+
+print("End of the Code")
+
+
+
+
+
---
+++ Classes and Objects/Program2.py
@@ -0,0 +1,26 @@
+
+
+# class Demo:
+
+#     def fun(self):
+#         print("in fun()")
+
+#     def fun(self, x):
+#         print("X : ", x)
+
+
+# Demo().fun()
+        
+
+def fun():
+    print("In Simple fun")
+
+def fun(x):
+    print("fun with x : ", x)
+
+fun(10) # fun not found bcoz the fun() is the replaces by the fun(x) so fun() is 
+
+fun()
+
+
+
