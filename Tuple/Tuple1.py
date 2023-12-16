24301838c08a2022a99243f6ccb4a543899556a3
VikasKare <karevikas7777@gmail.com>
2023-12-16 13:48:16 +0530

    Tuple Implematation and Methods

---
+++ Tuple/Tuple1.py
@@ -0,0 +1,22 @@
+
+lang = ("Java", "Python", "Dart", "Flutter", ("CPP", "Bhailang"))
+
+print(lang)
+print([4][0])
+
+# lang[1] = "ML" # TypeError: 'tuple' object does not support item assignment
+
+print(lang)
+print(len(lang))
+print(id(lang))
+print(type(lang))
+print(lang.count("Java"))
+print(lang.index("Dart"))
+
+myPackagedTuple = 10,20,30,40
+print(myPackagedTuple)
+a,b,c,d = myPackagedTuple
+print(a,b,c,d)
+
+
+
